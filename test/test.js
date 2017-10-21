4b5430e226cc383b5410b25758db567d935381d8
James Wang <sambob12@gmail.com>
2017-10-20 21:59:50 -0400

    add trivial test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('useless', function() {
+	describe('trival', function() {
+		it('should always return true', function() {
+			assert.equal(1, 1);		
+		});
+	});
+});
