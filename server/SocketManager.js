329ae73f8d3e0775e3f577e2ace9c4857fa1a194
James Wang <sambob12@gmail.com>
2017-11-29 04:55:52 -0500

    set up socket rooms

--- server/SocketManager.js
+++ server/SocketManager.js
@@ -1,11 +1,22 @@
 
 export default class SocketManager {
-	constructor(io) {
-		this.io = io;
+    constructor(io) {
+        this.io = io;
+        this.clients = []; // super hacky - replace with redis TODO
+        this.io.on('connection', (socket) => { // change to different event TODO
+            this.handleGameConnection(socket); 
 
-		this.io.on('connection', (socket) =>{
-			console.log("connection");
-		});
-	}
+            socket.on('disconnect', () => {
+                console.log('Got disconnect!');
+                var i = this.clients.indexOf(socket);
+                this.clients.splice(i, 1);
+            }); 
+        });
+    }
+    handleGameConnection(socket) {
+        socket.join('testroom'); // replace with generated room name TODO
+        this.clients.push(socket);
+        this.io.to('testroom').emit("newplayer", this.clients.length);
+    }
 
 }
